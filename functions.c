62a9873eaa5ecf8b5496e98f791ccd3578fbaf9d
Richard-Biju <123318712+Richard-Biju@users.noreply.github.com>
2023-02-14 21:02:26 +0000

    week6

---
+++ functions.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "helpers.h"
+#include <stdbool.h>
+
+float calculate_tax(float price, float tax_rate);
+
+int main(int argc, char *argv[])
+{
+
+    printf("the number of arguments was %d\n", argc);
+
+    for(int i =0; i < argc; i++){
+
+    printf("Argument #%d was  %s\n", i+1, argv[i]);
+}
+
+float user_tax_rate = get_float("what is the tax rate in your area?");
+
+float user_price = get_float("what is the price of the item you want the tax calculated on?");
+
+float item_tax = calculate_tax(user_price, user_tax_rate);
+
+printf("the tax on this item would be $%f\n", item_tax);
+
+    return 0;
+}
+
+float calculate_tax(float price, float tax_rate);{
+
+    float tax = price * tax_rate;
+
+    return tax;
+    
+}
